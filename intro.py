5df2a51fd194e759a5317cdb18f5cbce4baeffb1
Juan-91 <Juan.Montiel@sdgku.edu>
2024-08-30 12:09:56 +0930

    Intro

---
+++ intro.py
@@ -0,0 +1,31 @@
+print("Hello from python")
+#console.log("Hello from python")
+
+#let variable=1;
+variable=1
+variable=True
+variable="Hello"
+
+array=["Juan", "Montiel", 3, 4, 5]
+print(array[4])
+array.append(6)
+print(array)
+array.pop()
+print(array)
+
+dict={
+    "name":"Juan",
+    "last_name":"Montiel"
+}
+print(dict)
+
+
+# for(i=0;i<size;i++){
+# let temp=array[i]
+# console.log("temp")}
+for numbers in array:
+    print("Juan")
+
+def Hello():
+    print("Hello from python function")
+Hello()
